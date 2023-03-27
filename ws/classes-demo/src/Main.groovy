f61bde85d3c58100ca942a0ba62becd3fb8fcaae
Praveen Reddy <praveen@Praveens-MacBook-Pro.local>
2023-03-27 13:03:06 +0530

    classes added

--- ws/classes-demo/src/Main.groovy
+++ ws/classes-demo/src/Main.groovy
@@ -1,3 +1,12 @@
-static void main(String[] args) {
-  println "Hello world123!"
+class Abc{
+  int a=100; // if sttaic is not mentioned, then its instance
+  static int b=2000;
+
+  static void main(String[] args) {
+    //a=200;  // invalid
+    def obj1=new Abc(); // new operator always stores the object in 'Heap' memory
+    obj1.a=87;
+    Abc.b=5000; // valid
+    println "Hello world123!"
+  }
 }
